ba5e6c8147f1c9fd4a2143a9a52eba481b1ba57a
Harsh Shah <harshn08@gmail.com>
2023-07-25 13:52:10 -0700

    Update 20220817004034_001_initial_fruit_schema.js

--- migrations/schema/20220817004034_001_initial_fruit_schema.js
+++ migrations/schema/20220817004034_001_initial_fruit_schema.js
@@ -7,7 +7,7 @@ const USERID = process.env.USERID;
 console.log("THE USERID is: " + USERID);
 
 exports.up = async function(knex) {
- console.log("The USERID is inside schema creation: " + USER_ID);
+ console.log("The USERID is inside schema creation: " + USERID);
  await knex.schema.raw("CREATE DATABASE IF NOT EXISTS " + USERID);
  await knex.schema.raw("CREATE TABLE IF NOT EXISTS " + USERID + ".fruit(id varchar(100) PRIMARY KEY , name varchar(100), quantity varchar(11) null, description varchar(200) null)");
  await knex.schema.raw("CREATE TABLE IF NOT EXISTS " + USERID + ".fruitoutbox(id varchar(100) PRIMARY KEY , name varchar(100), quantity varchar(11) null, description varchar(200) null)");
